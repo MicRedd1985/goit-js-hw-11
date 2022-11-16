e1bf11466393715bde492db9082fda9804e3d155
MicRedd1985 <micredd1985@gmail.com>
2022-11-16 10:56:41 +0200

    complete 11 home task

---
+++ src/index.js
@@ -0,0 +1,115 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  docClose: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const button = document.querySelector('.load-more');
+
+let page;
+let searchValue;
+let totalShowed;
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  gallery.innerHTML = '';
+  const [input] = event.target;
+
+  page = 1;
+  totalShowed = 0;
+  searchValue = input.value;
+  fetchByName(searchValue, 1);
+});
+
+window.addEventListener('load', hideButton);
+
+gallery.addEventListener('click', event => event.preventDefault());
+
+button.addEventListener('click', () => {
+  fetchByName(searchValue, ++page);
+});
+
+async function fetchByName(name, page) {
+  try{
+    const response = await axios.get(makeURL(name, page));
+    const result = await response.data;
+    renderResult(result);
+  } catch (error){
+    renderError(error);
+  }
+}
+
+const apiKey = '31315940-fbb1061bb3bfe12c6324aab94';
+function makeURL(name, page){
+    return `https://pixabay.com/api/?key=${apiKey}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+}
+
+function renderResult(result) {
+  if (result.hits.length == 0) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    return;
+  }
+  if (page == 1) {
+    Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images.`);
+  }
+  totalShowed += result.hits.length;
+  if (totalShowed >= result.totalHits) {
+    Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
+  }
+  gallery.innerHTML += result.hits.map(createResult).join('');
+  totalShowed >= result.totalHits ? hideButton() : showButton();
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+  lightbox.refresh();
+}
+
+function createResult({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<a class='gallery__item' href="${largeImageURL}">
+    <div class='photo-card'>
+        <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+        <div class='info'>
+            <p class="info-item"><b>Likes </b>${likes}</p>
+            <p class="info-item"><b>Views </b>${views}</p>
+            <p class="info-item"><b>Comments </b>${comments}</p>
+            <p class="info-item"><b>Downloads </b>${downloads}</p>
+        </div>
+    </div>
+    </a>`;
+}
+
+function renderError(result) {
+  Notiflix.Notify.failure('Something goes wrong! ' + result.message);
+}
+
+function hideButton(){
+  button.style.display = 'none';
+  button.style.visibility = 'hidden';
+}
+
+function showButton(){
+  button.style.display = 'block';
+  button.style.visibility = 'visible';
+}
